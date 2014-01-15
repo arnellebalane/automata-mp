5ae86776f95eb3d0be9688b94e67f85918f18659
Arnelle Balane <arnellebalane@gmail.com>
2014-01-15 14:12:45 +0800

    language mp - function parser

--- src/mp/language/Declaration.java
+++ src/mp/language/Declaration.java
@@ -2,11 +2,39 @@
 package mp.language;
 
 public class Declaration {
-  private final String type;
+  private String originalDeclaration;
+  private String[] validDataTypes = {"int", "double", "char"};
+  private final String declarationType;
+  private String returnType;
+  private String identifier;
+  private String[] parameters;
 
   public Declaration(String declaration) {
-    type = (declaration.indexOf("=") >= 0) ? "variable" : "function";
+    originalDeclaration = declaration;
+    if (declaration.indexOf("=") == -1 && declaration.indexOf("(") >= 0) {
+      declarationType = "function";
+      parseFunction(declaration);
+    } else {
+      declarationType = "variable";
+      parseVariable(declaration);
+    }
+  }
 
+  private void parseVariable(String declaration) {
+
+  }
+
+  private void parseFunction(String declaration) {
+    int parametersStart = declaration.indexOf("(") + 1;
+    int parametersEnd = declaration.indexOf(")");
+    parameters = declaration.substring(parametersStart, parametersEnd).split(",");
+    declaration = declaration.substring(0, parametersStart - 1);
+    int separationIndex = declaration.lastIndexOf(" ");
+    returnType = (separationIndex == -1) ? "" : declaration.substring(0, separationIndex);
+    identifier = (separationIndex == -1) ? declaration : declaration.substring(separationIndex + 1);
+    for (int i = 0; i < parameters.length; i++) {
+      parameters[i] = parameters[i].trim();
+    }
   }
 
   public boolean valid() {
