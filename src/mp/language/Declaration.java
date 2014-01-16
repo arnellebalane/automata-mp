02062b4d6f7fe5d0c7731be82ee0f7ff8defbadf
Arnelle Balane <arnellebalane@gmail.com>
2014-01-16 16:44:58 +0800

    language mp - remake; new function parser

--- src/mp/language/Declaration.java
+++ src/mp/language/Declaration.java
@@ -25,76 +25,23 @@ public class Declaration {
   }
 
   private void parseFunction(String declaration) {
-    int parametersStart = declaration.indexOf("(") + 1;
+    int parametersStart = declaration.indexOf("(");
     int parametersEnd = declaration.indexOf(")");
-    String parametersString = declaration.substring(parametersStart, parametersEnd).trim();
+    String parametersString = declaration.substring(parametersStart + 1, parametersEnd).trim();
     parameters = (parametersString.length() == 0) ? null : parametersString.split(",");
-    declaration = declaration.substring(0, parametersStart - 1);
-    int separationIndex = declaration.lastIndexOf(" ");
-    returnType = (separationIndex == -1) ? "" : declaration.substring(0, separationIndex).trim();
-    identifier = (separationIndex == -1) ? declaration : declaration.substring(separationIndex + 1).trim();
+    declaration = declaration.substring(0, parametersStart);
+    int typeSeparationIndex = declaration.lastIndexOf("*");
+    if (typeSeparationIndex < 0) {
+      typeSeparationIndex = declaration.lastIndexOf(" ");
+    }
+    returnType = (typeSeparationIndex < 0) ? "" : declaration.substring(0, typeSeparationIndex + 1).trim();
+    identifier = declaration.substring(typeSeparationIndex + 1);
+
+    System.out.println(returnType);
+    System.out.println(identifier);
     for (int i = 0; parameters != null && i < parameters.length; i++) {
       parameters[i] = parameters[i].trim();
+      System.out.println(parameters[i]);
     }
-  }
-
-  public boolean valid() {
-    return declaration.charAt(declaration.length() - 1) == ';'
-            && (returnType.equals("") || validDataType(returnType))
-            && validIdentifier(identifier) && validParameters();
-  }
-
-  private boolean validDataType(String dataType) {
-    int pointerSeparationIndex = dataType.indexOf("*");
-    int arraySeparationIndex = dataType.indexOf("[");
-    boolean validAsterisks = true;
-    boolean validBrackets = true;
-    if (pointerSeparationIndex >= 0) {
-      String asterisks = dataType.substring(pointerSeparationIndex);
-      dataType = dataType.substring(0, pointerSeparationIndex);
-      for (int i = 0; i < asterisks.length() &&  validAsterisks; i++) {
-        if (asterisks.charAt(i) != '*') {
-          validAsterisks = false;
-        }
-      }
-    }
-    if (arraySeparationIndex >= 0) {
-      String brackets = dataType.substring(arraySeparationIndex);
-      dataType = dataType.substring(0, arraySeparationIndex);
-      validBrackets = brackets.length() % 2 == 0;
-      for (int i = 0; i < brackets.length() && validBrackets; i++) {
-        if ((i % 2 == 0 && brackets.charAt(i) != '[') || (i % 2 == 1 && brackets.charAt(i) != ']')) {
-          validBrackets = false;
-        }
-      }
-    }
-    return validDataTypes.contains(dataType) && validAsterisks && validBrackets;
-  }
-
-  private boolean validIdentifier(String identifier) {
-    String characters = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String digits = "0123456789";
-    boolean validIdentifier = digits.indexOf(identifier.charAt(0)) == -1;
-    for (int i = 01; i < identifier.length() && validIdentifier; i++) {
-      if (characters.indexOf(identifier.charAt(i)) == -1 && digits.indexOf(identifier.charAt(i)) == -1) {
-        validIdentifier = false;
-      }
-    }
-    return !validDataTypes.contains(identifier) && validIdentifier;
-  }
-
-  private boolean validParameters() {
-    boolean validParameters = true;
-    for (int i = 0; parameters != null && i < parameters.length && validParameters; i++) {
-      if (parameters[i].indexOf(" ") == -1) {
-        validParameters = validDataType(parameters[i]);
-      } else {
-        int separationIndex = parameters[i].lastIndexOf(" ");
-        String parameterDataType = parameters[i].substring(0, separationIndex).trim();
-        String parameterIdentifier = parameters[i].substring(separationIndex + 1).trim();
-        validParameters = validDataType(parameterDataType) && validIdentifier(parameterIdentifier);
-      }
-    }
-    return validParameters;
   }
 }
--- src/mp/language/DeclarationTester.java
+++ src/mp/language/DeclarationTester.java
@@ -1,21 +1,8 @@
 
 package mp.language;
 
-import java.util.Iterator;
-import utilities.inputreader.InputReader;
-import utilities.nodelist.Node;
-import utilities.nodelist.NodeList;
-import utilities.outputwriter.OutputWriter;
-
 public class DeclarationTester {
   public static void main(String[] args) {
-    NodeList declarations = InputReader.read("inputs/mp/language/mp3.in");
-    OutputWriter writer = new OutputWriter("outputs/mp/language/mp3.out");
-
-    Iterator iterator = declarations.iterator();
-    while (iterator.hasNext()) {
-      Declaration declaration = new Declaration(((Node) iterator.next()).toString());
-      writer.write((declaration.valid()) ? "valid" : "invalid");
-    }
+    Declaration declaration = new Declaration("void main(int[] x);");
   }
 }
