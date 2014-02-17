1c99fbf1ef5da21346322a03c62a59dac150877b
Arnelle Balane <arnellebalane@gmail.com>
2014-02-17 21:56:55 +0800

    tokenizer combines certain operators

--- src/mp/dfa/Tokenizer.java
+++ src/mp/dfa/Tokenizer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Tokenizer {
   public static ArrayList tokenize(String input) {
     ArrayList tokens = new ArrayList();
-    String delimeters = " &|(){}[]<>,;+-*/=";
+    String delimeters = " &|(){}[]<>,;+-*/=!%";
     for (int i = 0; i < input.length(); i++) {
       String content = "" + input.charAt(i);
       if (delimeters.indexOf(content) < 0) {
@@ -15,6 +15,23 @@ public class Tokenizer {
           content += input.charAt(i++);
         }
         i--;
+      } else {
+        String next = (i < input.length() - 1) ? "" + input.charAt(i + 1) : "";
+        if (content.equals("&") && next.equals("&")
+                || content.equals("|") && next.equals("|")
+                || content.equals("+") && next.equals("+")
+                || content.equals("-") && next.equals("-")
+                || content.equals("=") && next.equals("=")
+                || content.equals("!") && next.equals("=")
+                || content.equals("<") && next.equals("=")
+                || content.equals(">") && next.equals("=")
+                || content.equals("+") && next.equals("=")
+                || content.equals("-") && next.equals("=")
+                || content.equals("*") && next.equals("=")
+                || content.equals("/") && next.equals("=")
+                || content.equals("%") && next.equals("=")) {
+          content += input.charAt(++i);
+        }
       }
       if (content.trim().length() > 0) {
         tokens.add(new Token(content));
