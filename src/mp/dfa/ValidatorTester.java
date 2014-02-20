0708d737c9a4aefb4db69d7d327db2def757f738
Arnelle Balane <arnellebalane@gmail.com>
2014-02-21 07:03:51 +0800

    dfa mp - writes output file

--- src/mp/dfa/ValidatorTester.java
+++ src/mp/dfa/ValidatorTester.java
@@ -3,18 +3,20 @@ package mp.dfa;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import utilities.outputwriter.OutputWriter;
 
 public class ValidatorTester {
   public static void main(String[] args) {
     ArrayList inputContents = InputReader.read("inputs/mp/dfa/input.in");
     inputContents.remove(0);
 
+    OutputWriter writer = new OutputWriter("outputs/mp/dfa/mp4.out");
     Iterator iterator = inputContents.iterator();
     while (iterator.hasNext()) {
       Validator validator = new Validator();
       String input = (String) iterator.next();
       boolean valid = validator.validate(input);
-      System.out.println((valid) ? "ACCEPTED" : "DENIED");
+      writer.write((valid) ? "ACCEPTED" : "DENIED");
     }
   }
 }
